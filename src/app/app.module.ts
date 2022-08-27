4db0aa55a4c185fdd7c1c5cb630fe1ba5c0db67a
AlxCass <alexcass1402@gmail.com>
2022-08-26 20:46:54 -0500

    Prueba Turing-IA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { Routes, RouterModule } from '@angular/router';
+import { appRoutes } from './components/app.routing';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { ModalDetalleServiciosComponent } from './components/servicios/modal-detalle-servicios/modal-detalle-servicios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BannerComponent,
+    FooterComponent,
+    NosotrosComponent,
+    ServiciosComponent,
+    ModalDetalleServiciosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
